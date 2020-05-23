c383629eea39dd800063601881ff9356613df519
BogdanBercea <bogdan.bercea98@yahoo.com>
2020-05-23 11:20:28 +0300

    SantaRun Game Added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject gameOverPannel;
+    public Text scoreText;
+    int score = 0;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+        ObstacleSpawner.instance.gameOver = true;
+        StopScrolling();
+        gameOverPannel.SetActive(true);
+    }
+
+    public void StopScrolling()
+    {
+        TextureScroll[] scrollingObjects = FindObjectsOfType<TextureScroll>();
+
+        foreach(TextureScroll textureScrollObj in scrollingObjects)
+        {
+            textureScrollObj.scroll = false;
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
